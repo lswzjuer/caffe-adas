522cd4266dc09cf0cdb126f1bd1f7711f6613de5
yaowanchao <yaowanchao@fabu.ai>
2018-09-10 11:43:54 +0800

    update

--- fabu/models/od/400_640_JDetNet/train.sh
+++ fabu/models/od/400_640_JDetNet/train.sh
@@ -3,6 +3,6 @@
 #-snapshot=snapshot/ssdJacintoNetV2_iter_21000.solverstate \
 mkdir -p snapshot
 ~/ADAS_caffe/build/tools/caffe train -solver="solver.prototxt" \
--snapshot=snapshot/ssdJacintoNetV2_iter_3000.solverstate \
+-snapshot=snapshot/ssdJacintoNetV2_iter_95000.solverstate \
 -gpu 0,1,2,3,4,5,6  \
 2>&1 | tee log.txt.`date +'%Y-%m-%d_%H-%M-%S'`

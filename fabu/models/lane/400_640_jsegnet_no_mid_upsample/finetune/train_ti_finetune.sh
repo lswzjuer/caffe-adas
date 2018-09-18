f8b06599a2ece287eb471e6c345e5d5d88e9d733
yaowanchao <yaowanchao@fabu.ai>
2018-09-18 09:27:28 +0800

    finetune

--- fabu/models/lane/400_640_jsegnet_no_mid_upsample/finetune/train_ti_finetune.sh
+++ fabu/models/lane/400_640_jsegnet_no_mid_upsample/finetune/train_ti_finetune.sh
@@ -4,5 +4,5 @@ set -e
     #-snapshot=./snapshot/ti_sim_finetune_iter_15000.solverstate \
  #~/caffe-ssd/build/tools/caffe train \
  ~/ADAS_caffe/build/tools/caffe train \
-    -snapshot=./snapshot/ti_sim_finetune_iter_46000.solverstate \
+    -weights=../../../od/400_640_JDetNet/ssdJacintoNetV2_iter_92000.caffemodel \
     --solver=solver_ti_finetune.prototxt --gpu 0,1,2,3,4,5,6,7 2>&1 | tee log_ti.txt.`date +'%Y-%m-%d_%H-%M-%S'`
